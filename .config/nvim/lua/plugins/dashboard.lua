cd24f033b3b0e365fd95f41dfa83c8f6a9f617c6
ndom91 <yo@ndo.dev>
2022-05-01 18:47:25 +0200

    chore: cleanup

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,46 +1,3 @@
--- vim.g.dashboard_custom_header = {
---   '  ',
---   '  ',
---   '  ',
---   '  ',
---   '  ',
---   '  ',
---   '                     ``        `````````````````````````````                                   ',
---   '                    :md-     .ymNNNNNNNNNNNNNNNNNNNNNNNNNmdhy+/.`                              ',
---   '                  `oNNNNo`   sNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmy/.                           ',
---   '                 :dNNNNNNh.  +NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNh+`                        ',
---   '               `oNNNNNNNNNm+  :mNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNs.                      ',
---   '              -hNNNNNNNNNNNNh- .yNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmo.                    ',
---   '            `oNNNNNNNNNNNNNNNm+` /mNNNNNNNNNNNNNoooooooosymNNNNNNNNNNNNNNNd/                   ',
---   '           `hNNNNNNNNNNNNNNNNNNy. .hNNNNNNNNNNNNo`        `./sdNNNNNNNNNNNNNo                  ',
---   '           /NNNNNNNNNNNNNNNNNNNNm+ `+mNNNNNNNNNNNd-           `:hNNNNNNNNNNNNo                 ',
---   '           /NNNNNNNNNNNNNNNNNNNNNNy` -hNNNNNNNNNNNN+`           `/hNNNNNNNNNNNo                ',
---   '           /NNNNNNNNNNomNNNNNNNNNNNd/ `+NNNNNNNNNNNNy-            `sNNNNNNNNNNN-               ',
---   '           /NNNNNNNNNN..hNNNNNNNNNNNNs` -dNNNNNNNNNNNm+`            sNNNNNNNNNNd               ',
---   '           /NNNNNNNNNN. `omNNNNNNNNNNNd: .oNNNNNNNNNNNNy.            yNNNNNNNNNN:              ',
---   '           /NNNNNNNNNN.   -dNNNNNNNNNNNNo` :mNNNNNNNNNNNm/           -NNNNNNNNNNo              ',
---   '           /NNNNNNNNNN.     sNNNNNNNNNNNNh- `sNNNNNNNNNNNNs`          yNNNNNNNNNy              ',
---   '           /NNNNNNNNNN.      -hNNNNNNNNNNNN+  :mNNNNNNNNNNNm:         sNNNNNNNNNh              ',
---   '           /NNNNNNNNNN.       `sNNNNNNNNNNNNh- `sNNNNNNNNNNNNs`       yNNNNNNNNNy              ',
---   '           /NNNNNNNNNN.         :mNNNNNNNNNNNN+  /mNNNNNNNNNNNm-     -NNNNNNNNNNo              ',
---   '           /NNNNNNNNNN.          `sNNNNNNNNNNNNy. .sNNNNNNNNNNNN+`   yNNNNNNNNNN:              ',
---   '           /NNNNNNNNNN.            :dNNNNNNNNNNNm+  /mNNNNNNNNNNNh.`sNNNNNNNNNNd               ',
---   '           /NNNNNNNNNN.             `yNNNNNNNNNNNNy. .yNNNNNNNNNNNmhNNNNNNNNNNN-               ',
---   '           /NNNNNNNNNN.              `/mNNNNNNNNNNNm: `+NNNNNNNNNNNNNNNNNNNNNNo                ',
---   '           /NNNNNNNNNN.                .hNNNNNNNNNNNNy. -hNNNNNNNNNNNNNNNNNNNo                 ',
---   '           `sNNNNNNNNN.                 `+mNNNNNNNNNNNmh+/hNNNNNNNNNNNNNNNNN+                  ',
---   '             /mNNNNNNN.                   -hNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNd:                   ',
---   '              .sNNNNNN.                     +NNNNNNNNNNNNNNNNNNNNNNNNNNNNNy`                   ',
---   '                /mNNNN.                      :dNNNNNNNNNNNNNNNNNNNNNNNNNNNNh-                  ',
---   '                 .yNNN.                       `oNNNNNNNNNNNNNNNNNNNNNNNNNNNNN+                 ',
---   '                   +NN.                         -hNNNNNNNNNNNNNNNydNNNNNNNNNNNy.               ',
---   '                    .h.                           .+hdmNNmdhyo:.  `sdmmmmmmmmmmd-',
---   '  ',
---   '  ',
---   '  ',
---   '  ',
---   '  ',
--- }
 local ascii = {
   [[          ____                                         ]],
   [[         /___/\_                                       ]],
@@ -53,14 +10,6 @@ local ascii = {
   [[       \_\/_/\       /_\_\/       \/_/\/_/ \/__/    \/_/ \/_/\/_/\/_/]],
   [[          \_\/       \_\/                              ]]
 }
--- local nvim = [
---  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
---  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
---  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
---  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
---  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
---  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
--- ]
 vim.g.dashboard_custom_header = ascii
 vim.g.indentLine_fileTypeExclude = { 'dashboard' }
 vim.g.dashboard_default_executive = "telescope.nvim"
