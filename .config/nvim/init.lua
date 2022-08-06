54f765418f319c5d8f9505ec88d7352afc8e3c38
ndom91 <yo@ndo.dev>
2022-08-06 22:10:19 +0200

    fix: nvim init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,12 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
--- autoreload when editing init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', { command = 'source <afile> | PackerCompile', group = packer_group, pattern = '~/.config/nvim/**/*.lua' })
+-- local fn = vim.fn
+-- local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+-- if fn.empty(fn.glob(install_path)) > 0 then
+--   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+-- end
+--
+-- -- autoreload when editing init.lua
+-- local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+-- vim.api.nvim_create_autocmd('BufWritePost', { command = 'source <afile> | PackerCompile', group = packer_group, pattern = '~/.config/nvim/**/*.lua' })
 
 -- make sure that globals.lua is required first, as we want to use the
 -- functions and helpers we add there EVERYWHERE in our configuration
@@ -17,6 +17,6 @@ require 'globals'
 -- introduce a bug on accident, its likely that the rest of your config works
 -- fine other than some plugin configuration that is going awry
 require 'settings'
-require 'plugins'
+require('plugins').setup()
 require 'mappings'
 require 'autocmds'
