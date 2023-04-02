562b9641c7fd87f2bd09e33be02d118dbf58e8d7
ndom91 <yo@ndo.dev>
2023-04-02 17:11:43 +0200

    feat: hyprland + wayland configs

---
+++ install-hyperland.sh
@@ -0,0 +1,7 @@
+yay -S hyprland-bin waybar-hyprland \
+	swaybg swaylock-effects wofi wofi-emoji wlogout mako \
+	ttf-jetbrains-mono-nerd noto-fonts-emoji \
+	polkit-gnome python-requests starship \
+	swappy grim slurp pamixer brightnessctl inotify-tools \
+	bluez bluez-utils lxappearance xfce4-settings \
+	dracula-gtk-theme dracula-icons-git xdg-desktop-portal-hyprland-git
