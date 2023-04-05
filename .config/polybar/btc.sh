6733b5064b4c65c745b83471a50aafa737da5984
ndom91 <yo@ndo.dev>
2023-04-05 14:02:37 +0200

    fix: polybar scripts

--- .config/polybar/btc.sh
+++ .config/polybar/btc.sh
@@ -3,7 +3,8 @@
 LC_NUMERIC="en_US.UTF-8"
 
 # Get current Price from rate.sx
-RATE=$(curl -s eur.rate.sx/1btc)
+# RATE=$(curl -s eur.rate.sx/1btc)
+RATE=$(curl -s https://blockchain.info/ticker | jq ."EUR"."last")
 
 # Round it off
 RATE=$(printf "%.2f\n" "$RATE")
