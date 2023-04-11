275f6095e291fc36f0689052976977f5d3c50341
ndom91 <yo@ndo.dev>
2023-04-11 21:50:14 +0200

    fix: hyprland/waybar symlinked confs

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -1,10 +1,13 @@
-yay -S hyprland-bin waybar-hyprland \
+# yay -S hyprland-bin waybar-hyprland \
+yay -S hyprland-hidpi-xprop-git waybar-hyprland \
 	swaybg swaylock-effects wofi wofi-emoji wlogout mako \
 	noto-fonts-emoji wl-clipboard-history-git nwg-look-bin \
 	polkit-gnome python-requests starship \
 	swappy grim slurp pamixer brightnessctl inotify-tools \
 	bluez bluez-utils lxappearance xfce4-settings \
-	dracula-gtk-theme dracula-icons-git xdg-desktop-portal-hyprland-git
+	dracula-gtk-theme dracula-icons-git \
+	xdg-desktop-portal-hyprland-git \
+	xsettingsd xorg-xsetroot
 
 # hidpi
 # hyprland-hidpi-xprop-git
