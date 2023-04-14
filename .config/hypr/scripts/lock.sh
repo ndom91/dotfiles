ca1ebbbdde6821be8c1fe10fe3dffe87ea215fcd
ndom91 <yo@ndo.dev>
2023-04-15 00:42:54 +0200

    fix: swaylock

---
+++ .config/hypr/scripts/lock.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+swaylock \
+  --screenshots \
+  --clock \
+  --indicator \
+  --indicator-radius 150 \
+  --indicator-thickness 10 \
+  --effect-blur 7x5 \
+  --effect-vignette 0.5:0.5 \
+  --ring-color c4a7e7 \
+  --key-hl-color f6c177 \
+  --line-color 21202e \
+  --inside-color 191724 \
+  --separator-color 21202e \
+  --grace 2 \
+  --fade-in 0.5
