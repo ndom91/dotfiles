d9cf23ac7919304a5cc85bd32229d2348252b876
ndom91 <yo@ndo.dev>
2023-04-14 02:09:00 +0200

    feat: flemozi emoji/gif picker

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -7,7 +7,7 @@ yay -S hyprland-hidpi-xprop-git waybar-hyprland \
 	bluez bluez-utils lxappearance xfce4-settings \
 	dracula-gtk-theme dracula-icons-git \
 	xdg-desktop-portal-hyprland-git \
-	xsettingsd xorg-xsetroot imv
+	xsettingsd xorg-xsetroot imv flemozi
 
 # hidpi
 # hyprland-hidpi-xprop-git
