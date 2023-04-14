d388810acff855ac3d33eb1724f87b0a10c00e83
ndom91 <yo@ndo.dev>
2023-04-14 21:25:19 +0200

    fix: huawei hypr/waybar configs

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -1,13 +1,13 @@
 # yay -S hyprland-bin waybar-hyprland \
 yay -S hyprland-hidpi-xprop-git waybar-hyprland \
-	swaybg swaylock-effects wofi wofi-emoji wlogout mako \
+	swaybg swaylock-effects wofi wlogout mako \
 	noto-fonts-emoji cliphist nwg-look-bin \
 	polkit-gnome python-requests starship \
 	swappy grim slurp pamixer brightnessctl inotify-tools \
 	bluez bluez-utils lxappearance xfce4-settings \
 	dracula-gtk-theme dracula-icons-git \
 	xdg-desktop-portal-hyprland-git \
-	xsettingsd xorg-xsetroot imv flemozi \
+	xsettingsd xorg-xsetroot imv flemozi-bin \
 	swayidle bibata-cursor-theme-bin \
 	wf-recorder swayosd-git
 
