08147582e00d55cd7394f108751fdeb37f3590e6
ndom91 <yo@ndo.dev>
2023-09-03 19:35:51 +0200

    feat: update tmux statusline customization

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -9,6 +9,7 @@ yay -S hyprland-hidpi-xprop-git waybar-hyprland \
 	xsettingsd xorg-xsetroot imv flemozi-bin \
 	swayidle bibata-cursor-theme-bin \
 	wf-recorder swayosd-git wlsunset \
+  python-neovim-git python-libtmux \
 	catppuccin-gtk-theme-mocha blueberry-wayland \
 
 # hidpi
