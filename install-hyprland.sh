3ced560519fcf2ae403f486dc551be9d933ef98e
ndom91 <yo@ndo.dev>
2023-12-07 21:00:37 +0100

    feat: move to satty screenshot annotation tool

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -2,7 +2,7 @@ yay -S hyprland-hidpi-xprop-git waybar-hyprland \
 	swaybg swaylock-effects rofi-lbonn-wayland-git wlogout swaync \
 	noto-fonts-emoji cliphist nwg-look-bin \
 	polkit-gnome python-requests starship \
-	swappy grim slurp pamixer brightnessctl inotify-tools \
+	swappy satty-bin grim slurp pamixer brightnessctl inotify-tools \
 	bluez bluez-utils \
 	dracula-gtk-theme dracula-icons-git \
 	xdg-desktop-portal-hyprland-git \
