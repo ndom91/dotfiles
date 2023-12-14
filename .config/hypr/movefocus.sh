6e48e81637c03e2694696c3c01327d1a378dc2a8
ndom91 <yo@ndo.dev>
2023-12-14 16:00:16 +0100

    feat: update hyprland hy3 movefocus.sh

--- .config/hypr/movefocus.sh
+++ .config/hypr/movefocus.sh
@@ -1,21 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Move focus script to workaround hy3 not supporting movefocus
 # across multiple monitors
+# Use: bind = $mainMod, L, exec, movefocus.sh l
+#      bind = $mainMod, R, exec, movefocus.sh r
 # see: https://github.com/outfoxxed/hy3/issues/2
 
-NowWindow="$(hyprctl activewindow -j | jaq ".address")"
-if [ "$1" == "right" ] ; then
-  hyprctl dispatch hy3:movefocus r && sleep 0.05
-  ThenWindow="$(hyprctl activewindow -j | jaq ".address")"
-  if [ "$NowWindow" == "$ThenWindow" ]; then
-    # hyprctl dispatch focusmonitor +1
-    hyprctl dispatch movefocus r 
-  fi
-elif [ "$1" == "left" ] ; then
-  hyprctl dispatch hy3:movefocus l && sleep 0.05
-  ThenWindow="$(hyprctl activewindow -j | jaq ".address")"
-  if [ "$NowWindow" == "$ThenWindow" ]; then
-    # hyprctl dispatch focusmonitor -1
-    hyprctl dispatch movefocus l 
-  fi
+NowWindow="$(hyprctl activewindow -j | jq ".address")"
+
+hyprctl dispatch hy3:movefocus "$1"  # && sleep 0.05
+ThenWindow="$(hyprctl activewindow -j | jq ".address")"
+if [ "$NowWindow" == "$ThenWindow" ]; then
+  hyprctl dispatch movefocus "$1"
 fi
