ae3a2b4c6042a40e5b895872a1e76419cb34ed26
ndom91 <yo@ndo.dev>
2024-02-13 17:23:38 +0100

    fix: cleanup lualine incl. macro recording status back again!

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -31,6 +31,44 @@ return {
     },
   },
   config = function()
+    local function show_macro_recording()
+      local recording_register = vim.fn.reg_recording()
+      if recording_register == "" then
+        return ""
+      else
+        return "Recording @" .. recording_register
+      end
+    end
+    vim.api.nvim_create_autocmd("RecordingEnter", {
+      callback = function()
+        require("lualine").refresh({
+          place = { "statusline" },
+        })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("RecordingLeave", {
+      callback = function()
+        -- This is going to seem really weird!
+        -- Instead of just calling refresh we need to wait a moment because of the nature of
+        -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+        -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+        -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+        -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+        local timer = vim.loop.new_timer()
+        timer:start(
+          50,
+          0,
+          vim.schedule_wrap(
+            function()
+              require("lualine").refresh({
+                place = { "statusline" },
+              })
+            end
+          )
+        )
+      end,
+    })
     require("lualine").setup({
       options = {
         theme = "catppuccin",
@@ -80,6 +118,10 @@ return {
             symbols = { added = "+", modified = "~", removed = "-" }, -- Changes the symbols used by the diff.
           },
           {
+            "macro-recording",
+            fmt = show_macro_recording,
+          },
+          {
             require("noice").api.status.search.get,
             cond = require("noice").api.status.search.has,
             color = { fg = "fg" },
@@ -87,15 +129,15 @@ return {
         },
         lualine_x = {
           {
-            "diagnostics",
-            sources = { "nvim_diagnostic" },
-            sections = { "error", "warn", "info", "hint" },
-            symbols = { error = " ", warn = " ", info = " ", hint = " " },
-            -- symbols = { error = "E", warn = "W", info = "I", hint = "H" },
-            colored = true, -- Displays diagnostics status in color if set to true.
-            update_in_insert = false, -- Update diagnostics in insert mode.
-            always_visible = false, -- Show diagnostics even if there are none.
+            function()
+              local clients = vim.lsp.get_clients({ bufnr = 0 })
+              if #clients == 0 then return " " .. #clients end
+              return "  " .. table.concat(vim.tbl_map(function(client) return client.name end, clients), ", ")
+            end,
           },
+        },
+        lualine_y = {
+          -- "progress",
           {
             "copilot",
             show_colors = true,
@@ -104,18 +146,21 @@ return {
               spinners = require("copilot-lualine.spinners").dots,
             },
           },
-          { -- lsp
-            function()
-              local clients = vim.lsp.get_clients({ bufnr = 0 })
-              if #clients == 0 then return " " .. #clients end
-              return "  " .. table.concat(vim.tbl_map(function(client) return client.name end, clients), ", ")
-            end,
+          {
+            "diagnostics",
+            padding = 0,
+            sources = { "nvim_diagnostic" },
+            sections = { "error", "warn" },
+            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+            -- symbols = { error = "E", warn = "W", info = "I", hint = "H" },
+            colored = false, -- Displays diagnostics status in color if set to true.
+            update_in_insert = false, -- Update diagnostics in insert mode.
+            always_visible = false, -- Show diagnostics even if there are none.
           },
-          -- "fileformat",
-        },
-        lualine_y = {
-          -- "location"
-          "progress",
+          {
+            "location",
+            padding = 1,
+          },
         },
         lualine_z = {
           -- "fileformat",
