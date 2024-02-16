18805c151c0b56c54c28285f3bf9837864d81528
ndom91 <yo@ndo.dev>
2024-02-16 15:05:58 +0100

    fix: extract lsp-saga config into own file

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,31 +2,6 @@ return {
   "nvim-tree/nvim-web-devicons",
   "nvim-lua/plenary.nvim",
   "nvim-lua/popup.nvim",
-  {
-    "nvimdev/lspsaga.nvim",
-    enabled = true,
-    event = "LspAttach",
-    keys = {
-      { "<leader>sf", "<cmd>Lspsaga finder<CR>", desc = "[S]ymbol [F]inder" },
-    },
-    -- config = function() require("lspsaga").setup({}) end,
-    opts = {
-      symbol_in_winbar = {
-        enable = false,
-      },
-      lightbulb = {
-        enable = true,
-        enable_in_insert = false,
-        sign = false,
-        sign_priority = 40,
-        virtual_text = false,
-      },
-    },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
 
   -- icons on completion
   "onsails/lspkind-nvim",
