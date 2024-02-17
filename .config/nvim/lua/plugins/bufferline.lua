5d2f3a477ca8a404a16df3fc3fdf645a9f66f29e
ndom91 <yo@ndo.dev>
2024-02-17 14:40:59 +0100

    fix: cleanup bufferline and harpoon

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -21,39 +21,56 @@ return {
   },
   opts = {
     options = {
-      -- indicator = { icon = "▎" },
+      themable = true,
+      indicator = { icon = "▎" },
       buffer_close_icon = "",
       modified_icon = "●",
       close_icon = "",
       left_trunc_marker = "",
       right_trunc_marker = "",
       right_mouse_command = "bdelete! %d",
-      max_name_length = 25,
+      max_name_length = 20,
       max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
       truncate_names = true,
-      tab_size = 22,
-      diagnostics = "nvim_lsp",
-      diagnostics_indicator = function(total_count, level, diagnostics_dict)
-        local s = ""
-        for kind, count in pairs(diagnostics_dict) do
-          s = string.format("%s %s %d", s, symbol_map[kind], count)
-        end
-        return s
-      end,
+      tab_size = 18,
+      diagnostics = false, -- "nvim_lsp",
+      -- diagnostics_indicator = function(total_count, level, diagnostics_dict)
+      --   local s = ""
+      --   for kind, count in pairs(diagnostics_dict) do
+      --     s = string.format("%s %s %d", s, symbol_map[kind], count)
+      --   end
+      --   return s
+      -- end,
       offsets = {
         {
           filetype = "neo-tree",
           text = function() return vim.fn.getcwd() end,
           text_align = "center",
+          separator = false,
         },
       },
+      hover = {
+        enabled = false,
+        delay = 200,
+        reveal = { "close" },
+      },
+      numbers = function(opts)
+        -- Show harpoon index numbers in bufferline tabs
+        local items = require("harpoon"):list().items
+        for i = 1, #items do
+          local fn = items[i].value
+          local fullpath = vim.fn.fnamemodify(fn, ":p")
+          local bufPathFromId = vim.fn.fnamemodify(vim.fn.bufname(opts.id), ":p")
+          if fullpath == bufPathFromId then return i end
+        end
+      end,
       color_icons = true,
       show_buffer_icons = true,
-      show_buffer_close_icons = true,
+      show_buffer_close_icons = false,
       show_close_icon = false,
-      show_tab_indicators = true,
-      separator_style = "thin", -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
-      enforce_regular_tabs = false,
+      show_tab_indicators = false,
+      separator_style = { "", "" },
+      enforce_regular_tabs = true,
       always_show_bufferline = true,
     },
   },
--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -4,29 +4,34 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  keys = {
+    { "<leader>a", function() require("harpoon"):list():append() end, "n", desc = "[A]dd" },
+    {
+      "<leader>h",
+      function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,
+      "n",
+      desc = "Harpoon List",
+    },
+    { "<C-p>", function() require("harpoon"):list():prev() end, "n", desc = "Harpoon [P]rev" },
+    { "<C-n>", function() require("harpoon"):list():next() end, "n", desc = "Harpoon [N]ext" },
+  },
   config = function()
-    local harpoon = require "harpoon"
-    local extensions = require "harpoon.extensions"
+    local harpoon = require("harpoon")
+    local extensions = require("harpoon.extensions")
 
-    harpoon:setup {}
-
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-    -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+      },
+    })
 
     harpoon:extend(extensions.builtins.navigate_with_number())
-    harpoon:extend {
+    harpoon:extend({
       UI_CREATE = function(cx)
-        vim.keymap.set(
-          "n",
-          "<M-v>",
-          function() harpoon.ui:select_menu_item { vsplit = true } end,
-          { buffer = cx.bufnr }
-        )
+        vim.keymap.set("n", "S", function() harpoon.ui:select_menu_item({ vsplit = true }) end, { buffer = cx.bufnr })
+        vim.keymap.set("n", "s", function() harpoon.ui:select_menu_item({ split = true }) end, { buffer = cx.bufnr })
       end,
-    }
+    })
   end,
 }
