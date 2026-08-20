4d244ded6be55502a04bb9ce6d6b399fdd9072a8
ndom91 <yo@ndo.dev>
2026-08-20 13:56:32 +0200

    Update minuet-ai to use ornith-35b-a3b

--- .config/nvim/lua/plugins/minuet-ai.lua
+++ .config/nvim/lua/plugins/minuet-ai.lua
@@ -18,7 +18,7 @@ return {
           end,
           name = 'llama.cpp',
           end_point = 'http://llama-dash.puff.lan/v1/chat/completions',
-          model = 'qwen3.6-35b',
+          model = 'ornith-35b-a3b',
           optional = {
             max_tokens = 64,
             temperature = 0.2,
